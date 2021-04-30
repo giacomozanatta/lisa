16d6bb56970398767bb41763129a8f99c1f82f91
Giacomo Zanatta <gzanatta@alpenite.com>
2021-04-30 18:57:51 +0200

    Add Bricks Domain
    Add Bricks Domain

---
+++ lisa/src/main/java/it/unive/lisa/analysis/nonrelational/value/impl/BricksDomain.java
@@ -0,0 +1,47 @@
+package it.unive.lisa.analysis.nonrelational.value.impl;
+
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+
+public class BricksDomain extends BaseNonRelationalValueDomain<BricksDomain> {
+
+    @Override
+    protected BricksDomain lubAux(BricksDomain other) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    protected BricksDomain wideningAux(BricksDomain other) throws SemanticException {
+        return null;
+    }
+
+    @Override
+    protected boolean lessOrEqualAux(BricksDomain other) throws SemanticException {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public BricksDomain top() {
+        return null;
+    }
+
+    @Override
+    public BricksDomain bottom() {
+        return null;
+    }
+
+    @Override
+    public String representation() {
+        return null;
+    }
+}
---
+++ lisa/src/test/java/it/unive/lisa/bricks/BricksDomainTest.java
@@ -0,0 +1,22 @@
+package it.unive.lisa.bricks;
+
+import it.unive.lisa.imp.ParsingException;
+import org.junit.Test;
+
+import it.unive.lisa.AnalysisException;
+import it.unive.lisa.LiSA;
+
+
+public class BricksDomainTest {
+
+    @Test
+    public void testBricks() throws ParsingException, AnalysisException {
+       /* LiSA lisa = new LiSA();
+        lisa.setDumpAnalysis(true);
+        lisa.setWorkdir("test-outputs/sign-domain");
+        lisa.setProgram(IMPFrontend.processFile("signs.imp"));
+        lisa.setAbstractState(LiSAFactory.getDefaultFor(AbstractState.class,
+                LiSAFactory.getDefaultFor(HeapDomain.class), new BricksDomain()));
+        lisa.run();*/
+    }
+}
