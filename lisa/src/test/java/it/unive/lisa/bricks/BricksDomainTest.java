60f350cbd1f9b9e6cbe88799bcead6845748a074
Giacomo Zanatta <gzanatta@alpenite.com>
2021-05-20 12:15:28 +0200

    fix  test class
    Fix test class

--- lisa/src/test/java/it/unive/lisa/bricks/BricksDomainTest.java
+++ lisa/src/test/java/it/unive/lisa/bricks/BricksDomainTest.java
@@ -1,22 +1,27 @@
 package it.unive.lisa.bricks;
 
+import it.unive.lisa.LiSA;
+import it.unive.lisa.LiSAConfiguration;
+import it.unive.lisa.LiSAFactory;
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.heap.HeapDomain;
+import it.unive.lisa.analysis.nonrelational.value.impl.BricksDomain;
+import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
 import org.junit.Test;
 
 import it.unive.lisa.AnalysisException;
-//import it.unive.lisa.LiSA;
 
 
 public class BricksDomainTest {
 
     @Test
     public void testBricks() throws ParsingException, AnalysisException {
-       /* LiSA lisa = new LiSA();
-        lisa.setDumpAnalysis(true);
-        lisa.setWorkdir("test-outputs/sign-domain");
-        lisa.setProgram(IMPFrontend.processFile("signs.imp"));
-        lisa.setAbstractState(LiSAFactory.getDefaultFor(AbstractState.class,
-                LiSAFactory.getDefaultFor(HeapDomain.class), new BricksDomain()));
-        lisa.run();*/
+        new LiSA(
+                new LiSAConfiguration().setDumpAnalysis(true)
+                .setWorkdir("test-outputs/sign-domain")
+                .setAbstractState(LiSAFactory.getDefaultFor(AbstractState.class,
+                                LiSAFactory.getDefaultFor(HeapDomain.class), new BricksDomain(null)))
+        ).run(IMPFrontend.processFile("imp-testcases/sign/program.imp"));
     }
 }
